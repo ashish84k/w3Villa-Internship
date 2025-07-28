8b62c330aa906dc13eb966099dbec76d53cd4ddd
ashish84k <ashishshen20@gmail.com>
2025-07-28 18:32:54 +0530

    add tailwind css project practical code

---
+++ Tailwind-Css/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./components/auth/Login";
+
+const App = () => {
+  return (
+    <div className="container w-full m-auto ">
+      <Login></Login>
+    </div>
+  );
+};
+
+export default App;
